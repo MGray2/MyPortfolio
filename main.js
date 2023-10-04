69cec63e59b35e31d01b760bff7c10fc8f548f46
unknown <mgray24@basecampcodingacademy.org>
2023-10-04 11:27:30 -0500

    new JavaScript Dark Mode implementation

---
+++ main.js
@@ -0,0 +1,33 @@
+const body = document.body;
+const aboutBox = document.getElementById("aboutMeBox");
+const darkMode = document.getElementById("darkButton");
+const portalBox = document.getElementById("linkButtonBox");
+const portalBox2 = document.getElementById("linkButtonBox2");
+const skillBox = document.getElementById("skillsBox");
+const skillBar = document.getElementById("iconHolder");
+darkMode.addEventListener("click", () => {
+  if (body.style.backgroundColor == "lightgray") {
+    darkMode.style.color = "gold";
+    body.style.backgroundColor = "black";
+    aboutBox.style.backgroundColor = "rgb(33, 33, 33)";
+    aboutBox.style.color = "white";
+    portalBox.style.backgroundColor = "rgb(33, 33, 33)";
+    portalBox.style.color = "white";
+    portalBox2.style.backgroundColor = "rgb(33, 33, 33)";
+    portalBox2.style.color = "white";
+    skillBox.style.backgroundColor = "rgb(33, 33, 33)";
+    skillBox.style.color = "white";
+    skillBar.style.backgroundColor = "white";
+  } else {
+    darkMode.style.color = "white";
+    body.style.backgroundColor = "lightgray";
+    aboutBox.style.backgroundColor = "white";
+    aboutBox.style.color = "black";
+    portalBox.style.backgroundColor = "white";
+    portalBox.style.color = "black";
+    portalBox2.style.backgroundColor = "white";
+    portalBox2.style.color = "black";
+    skillBox.style.backgroundColor = "white";
+    skillBox.style.color = "black";
+  }
+});
