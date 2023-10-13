d87053d3761f233458807d005d86382165ca6cc4
unknown <mgray24@basecampcodingacademy.org>
2023-10-13 13:52:52 -0500

    visual improvements and completed projects page

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const body = document.body;
 const aboutBox = document.getElementById("aboutMeBox");
 const darkMode = document.getElementById("darkButton");
-const portalBox = document.getElementById("linkButtonBox");
-const portalBox2 = document.getElementById("linkButtonBox2");
+const box = document.querySelectorAll(".linkButtonBox");
+const icons = document.querySelectorAll(".iconBlock");
+
 const skillBox = document.getElementById("skillsBox");
 const skillBar = document.getElementById("iconHolder");
 const aboutMe = document.querySelector("#aboutMeBox p");
@@ -10,26 +11,31 @@ const aboutMe = document.querySelector("#aboutMeBox p");
 darkMode.addEventListener("click", () => {
   if (body.style.backgroundColor == "lightgray") {
     darkMode.style.color = "gold";
-    body.style.backgroundColor = "black";
+    body.style.backgroundColor = "rgb(24, 24, 24)";
     aboutBox.style.backgroundColor = "rgb(33, 33, 33)";
     aboutBox.style.color = "white";
-    portalBox.style.backgroundColor = "rgb(33, 33, 33)";
-    portalBox.style.color = "white";
-    portalBox2.style.backgroundColor = "rgb(33, 33, 33)";
-    portalBox2.style.color = "white";
+    box[0].style.backgroundColor = "rgb(33, 33, 33)";
+    box[0].style.color = "white";
+    box[1].style.backgroundColor = "rgb(33, 33, 33)";
+    box[1].style.color = "white";
     skillBox.style.backgroundColor = "rgb(33, 33, 33)";
     skillBox.style.color = "white";
-    skillBar.style.backgroundColor = "white";
+    for (let i = 0; i < icons.length; i++) {
+      icons[i].style.filter = "invert(100%)";
+    }
   } else {
     darkMode.style.color = "white";
     body.style.backgroundColor = "lightgray";
     aboutBox.style.backgroundColor = "white";
     aboutBox.style.color = "black";
-    portalBox.style.backgroundColor = "white";
-    portalBox.style.color = "black";
-    portalBox2.style.backgroundColor = "white";
-    portalBox2.style.color = "black";
+    box[0].style.backgroundColor = "white";
+    box[0].style.color = "black";
+    box[1].style.backgroundColor = "white";
+    box[1].style.color = "black";
     skillBox.style.backgroundColor = "white";
     skillBox.style.color = "black";
+    for (let i = 0; i < icons.length; i++) {
+      icons[i].style.filter = "none";
+    }
   }
 });
